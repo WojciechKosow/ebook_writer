e49ef4924ff63d573ab71525d653784da9d599f0
wojte <wojciech@kosow.pl>
2026-08-17 17:29:12 +0200

    initial commit

---
+++ src/main/java/com/ebookwriter/SaaS/SaaSApplication.java
@@ -0,0 +1,13 @@
+package com.ebookwriter.SaaS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaaSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaaSApplication.class, args);
+	}
+
+}
